c1f32385a3ce53e04bad01f65dde0bdcde77ca43
Giselle bernal <giselle.bernal@adidas.com>
2022-12-21 10:42:40 -0500

    Video 263. pokemonSlice

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import './index.css';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
+import { PokemonApp } from './PokemonApp'; //Importacion el componente
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from './slices/counter/counterSlice';
+import { pokemonSlice } from './slices/Pokemon/pokemonSlice'; //Se agrego al llamar el pokemonSlice
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer, 
     //Esto ya puede venir con el reducer desde el slice y aqui no habria necesidad de colocar el .reducer
+    pokemons: pokemonSlice.reducer, /* Agregar pokemonSlice al store */
   },
 })
