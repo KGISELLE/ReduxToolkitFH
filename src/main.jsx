fd8bbc3327be48e3761498ab9a7f9a87354c1384
Giselle bernal <giselle.bernal@adidas.com>
2022-12-26 11:25:14 -0500

    Video 267. Primeros pasos RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 // import App from './App';
+// import { PokemonApp } from './PokemonApp'; //Importacion el componente
+import { TodoApp } from './TodoApp';
+
 import './index.css';
+
 import { store } from './store/store';
 import { Provider } from 'react-redux';
-import { PokemonApp } from './PokemonApp'; //Importacion el componente
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 )
